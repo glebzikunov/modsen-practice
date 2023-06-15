48d203ab3dd232f73908197b045689826b33a7a0
Gleb Zikunov <zikunovga@gmail.com>
2023-06-16 02:16:27 +0300

    refactor: move const variables into constants

---
+++ litera-search/src/constants/index.jsx
@@ -0,0 +1,18 @@
+export const BOOK_API_URL = import.meta.env.VITE_BOOK_API_URL;
+export const BOOK_API_KEY = import.meta.env.VITE_BOOK_API_KEY;
+export const MAX_RESULTS = "&maxResults=30";
+
+export const CATEGORIES = [
+  { value: "all", text: "All" },
+  { value: "art", text: "Art" },
+  { value: "biography", text: "Biography" },
+  { value: "computers", text: "Computers" },
+  { value: "history", text: "History" },
+  { value: "medical", text: "Medical" },
+  { value: "poetry", text: "Poetry" }
+];
+
+export const SORTINGS = [
+  { value: "relevance", text: "Relevance" },
+  { value: "newest", text: "Newest" },
+];
