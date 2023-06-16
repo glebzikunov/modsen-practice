f216c21b37c4ccf4db24d6a3abe7b8d7630ae4a8
Gleb Zikunov <zikunovga@gmail.com>
2023-06-16 20:19:39 +0300

    refactor: format the code

--- litera-search/src/components/BooksSection/index.jsx
+++ litera-search/src/components/BooksSection/index.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import BookCard from '../BookCard/index'
-import './styles.css'
+import React from "react"
+import BookCard from "../BookCard/index"
+import "./styles.css"
 
 const BooksSection = ({ searchValue }) => {
-  const numberOfBooks = searchValue.totalItems;
-  const bookArr = searchValue.items;
-  const searchResult = numberOfBooks === 1 ? 'Found 1 result' : `Found ${numberOfBooks} results`;
-  
+  const numberOfBooks = searchValue.totalItems
+  const bookArr = searchValue.items
+  const searchResult =
+    numberOfBooks === 1 ? "Found 1 result" : `Found ${numberOfBooks} results`
+
   return (
-    <section className='books-section'>
+    <section className="books-section">
       <div className="books-section-wrapper">
-        {searchValue && <h4 className='books-section-title'>{searchResult}</h4>}
-        <div className='books-container'>
-          {bookArr && (bookArr.map((book, index) => (
-            <BookCard
-              key={index}
-              bookCover={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail}
-              bookCategories={book.volumeInfo.categories && Array.from(book.volumeInfo.categories)[0]}
-              bookTitle={book.volumeInfo.title}
-              bookAuthors={book.volumeInfo.authors && Array.from(book.volumeInfo.authors).join(', ')}
-            />
-          )))}
+        {searchValue && <h4 className="books-section-title">{searchResult}</h4>}
+        <div className="books-container">
+          {bookArr &&
+            bookArr.map((book, index) => (
+              <BookCard
+                key={index}
+                bookCover={
+                  book.volumeInfo.imageLinks &&
+                  book.volumeInfo.imageLinks.smallThumbnail
+                }
+                bookCategories={
+                  book.volumeInfo.categories &&
+                  Array.from(book.volumeInfo.categories)[0]
+                }
+                bookTitle={book.volumeInfo.title}
+                bookAuthors={
+                  book.volumeInfo.authors &&
+                  Array.from(book.volumeInfo.authors).join(", ")
+                }
+              />
+            ))}
         </div>
       </div>
     </section>
   )
 }
 
-export default BooksSection;
+export default BooksSection
